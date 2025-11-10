2cf51d24b0930768876f6d9a797a958fe9e9186c
PkCa <bautistacacciavillani07@gmail.com>
2025-11-10 14:40:51 -0300

    clase rook

---
+++ pieces/rook.py
@@ -0,0 +1,9 @@
+from piece import Piece
+
+class Rook(Piece):
+    def __init__(self, color: str, col: str, row: int):
+        super().__init__("rook", color, col, row)
+
+    def moveStraight(self, vector_row: int, vector_col: int):
+        if vector_row == 0 or vector_col == 0:
+            super().move(vector_row=vector_row, vector_col=vector_col)
